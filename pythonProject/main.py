334a55293577b4de7189fcabec50ec922efa2102
JeremiahZero <digorec.91@gmail.com>
2020-08-25 21:38:00 +0300

    drawing

---
+++ pythonProject/main.py
@@ -0,0 +1,15 @@
+from graph import *
+
+brushColor("yellow")
+circle(250, 250, 100)
+brushColor('red')
+circle(200, 230, 20)
+circle(300, 230, 15)
+brushColor('black')
+circle(200, 230, 5)
+circle(300, 230, 5)
+rectangle(200, 305, 300, 320)
+polygon([(150, 160), (148, 162),
+         (218, 222), (220, 220)])
+
+run()
